b71cd383af99a2f27dbb1ac07f6c13d8a60df746
Bashar Umar <basharu83@gmail.com>
2024-03-23 22:27:39 +0100

    chunking the video

--- resources/views/livewire/upload-video.blade.php
+++ resources/views/livewire/upload-video.blade.php
@@ -1,7 +1,24 @@
 <x-mary-modal  wire:model="modal2" title="Upload Video">
+    <form x-data="{
+        uploder:null,
+        progress:0,
+        submit(){
+            const file = $refs.file.files[0]
+            console.log(file)
+        }
+    }">
+        <div>
+            <label class="flex w-full h-40 border-2 border-gray-400 border-dashed justify-center items-center"
+            for="video">
+                <span>
+                    <x-mary-icon name="fas.upload" label="Upload Video"> </x-mary-icon>
+                </span>
+                <input type="file" x-on:change.prevent="submit" x-ref="file" class="hidden" id="video">
+            </label>
+        </div>
+    </form>
     <x-slot:actions>
-        <x-mary-button label="Cancel" @click="$wire.modal = false" />
-        <x-mary-button label="Confirm" class="btn-primary" />
+
     </x-slot:actions>
 
 </x-mary-modal>
