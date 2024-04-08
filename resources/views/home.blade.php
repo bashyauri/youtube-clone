b2dae6cb37bffc5e4440bf885ec027c5b06ae920
Bashar Umar <basharu83@gmail.com>
2024-04-08 15:31:11 +0100

    create a video card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,9 @@
 <x-app-layout>
-Home
+    <div class="py-12 max-w-7xl mx-auto">
+        <div class="grid gap-x-8 gap-y-4 grid-cols-3">
+            @foreach ($videos as $video)
+                <livewire:video-card lazy :video="$video" :key="$video->id"/>
+            @endforeach
+        </div>
+    </div>
 </x-app-layout>
---
+++ resources/views/livewire/video-card.blade.php
@@ -0,0 +1,43 @@
+<div x-data="{playing: false}">
+    <div class="col-span-4">
+        <x-mary-card>
+            <x-slot:figure>
+                <video x-ref="player"
+                       @mouseenter="playing = true; $refs.player.play"
+                       @mouseleave="playing = false; $refs.player.pause();$refs.player.currentTime = 0"
+                       id="player"
+                       muted
+                       loop
+                       poster="storage/{{ $video->thumbnail_path ?? '' }}"
+                       data-poster="storage/{{ $video->thumbnail_path ?? '' }}">
+                    @foreach ($video->formats as $format)
+                        <source src="storage/{{$format->file_path}}" type="video/mp4" size="{{$format->quality}}" />
+                    @endforeach
+                </video>
+            </x-slot:figure>
+            <div class="space-y-2">
+                <div class="flex justify-between items-center">
+                    <div class="flex-grow-1 w-[150px]">
+                        <img src="{{ $video->user->profile_photo_url }}" alt="profile" class="rounded-full w-10"/>
+
+                    </div>
+                    <a href="#" wire:navigate class="flex flex-col space-y-1.5">
+                        <h2 class="text-2xl flex-wrap">
+                            {{ $video->title }}
+                        </h2>
+                        <div>
+                            {{ $video->user->name }}
+                            <span class="border-l-2 h-4 border-gray-300 ml-2 mr-2"></span>
+                            {{-- {{ Number::abbreviate(views($video)->unique()->count()) }} --}}
+                            2K
+                            <span class="border-l-2 h-4 border-gray-300 ml-2 mr-2"></span>
+                            <time datetime="{{ $video->created_at }}" title="{{ $video->created_at }}">
+                                {{ $video->created_at->diffForHumans() }}
+                            </time>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </x-card>
+    </div>
+</div>
