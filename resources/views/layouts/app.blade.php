8d741cd40ae8116319661be4c21b98e38c6d8b19
Bashar Umar <basharu83@gmail.com>
2024-04-09 16:02:41 +0100

    install plyr

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.css" />
---
+++ resources/views/livewire/player.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <video id="player"  playsinline controls data-poster="{{ $video->thumbnail_path }}">
+        @foreach ($video->formats as $format)
+        <source src="storage/{{$format->file_path}}" type="video/mp4" size="{{$format->quality}}">
+
+        @endforeach
+    </video>
+</div>
+@script
+<script>
+    const player = new Plyr('#player', {
+        previewThumbnails: {
+            enabled: true,
+            src: '{{ $video->vtt_file }}',
+        }
+    });
+</script>
+@endscript
